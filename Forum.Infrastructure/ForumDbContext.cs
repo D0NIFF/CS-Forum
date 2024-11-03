465149fd7465809ead009b7aa15283e7a8957633
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-03 19:43:16 +0300

    Create a db abstraction

---
+++ Forum.Infrastructure/ForumDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Forum.Infrastructure.Configuration;
+using Forum.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Infrastructure;
+
+public class ForumDbContext : DbContext
+{
+
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<PostCategoryEntity> PostCategories { get; set; }
+    public DbSet<PostEntity> Posts { get; set; }
+    public DbSet<CommentEntity> Comments { get; set; }
+
+    public ForumDbContext(DbContextOptions<ForumDbContext> options)
+        : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new PostCategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
+        modelBuilder.ApplyConfiguration(new CommentConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Forum.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,78 @@
+﻿using Forum.Application.Models;
+using Forum.Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Infrastructure.Repositories;
+
+public class UsersRepository : IUsersRepository
+{
+    private readonly ForumDbContext _context;
+
+    public UsersRepository(ForumDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// Get one user by ID
+    /// </summary>
+    /// <param name="id">User ID</param>
+    /// <returns></returns>
+    public async Task<User> GetUserById(Guid id)
+    {
+        var user = await _context.Users
+            .Where (u => u.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        return User.CreateFromEntity(user);
+    }
+
+    public async Task<User> GetUserByNickname(string nickname)
+    {
+        var user = await _context.Users
+            .Where(x => x.Nickname == nickname)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        return User.CreateFromEntity(user);
+    }
+
+    public async Task<User> GetUserByEmail(string email)
+    {
+        var user = await _context.Users
+            .Where(x => x.Email == email)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        return User.CreateFromEntity(user);
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsers()
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .Select(u => User.CreateFromEntity(u))
+            .ToListAsync();
+    }
+
+    public async Task<User> CreateUser(User user)
+    {
+
+    }
+
+    public async Task<User> UpdateUser(User user)
+    {
+
+    }
+
+    public async Task<User> UpdateUserById(Guid id, User user)
+    {
+
+    }
+
+    public async Task<User> DeleteUser(Guid id)
+    {
+
+    }
+}
