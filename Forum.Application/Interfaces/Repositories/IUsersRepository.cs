f7fb560f2746109169bc5cd5257c4f9ea71dce8e
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-03 19:43:02 +0300

    Create Interfaces

---
+++ Forum.Application/Interfaces/Repositories/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Forum.Application.Models;
+
+namespace Forum.Application.Interfaces.Repositories;
+
+public interface IUsersRepository
+{
+    Task<User> GetUserById(Guid id);
+    Task<User> GetUserByNickname(string nickname);
+    Task<User> GetUserByEmail(string email);
+    Task<IEnumerable<User>> GetAllUsers();
+    Task<User> CreateUser(User user);
+    Task<User> UpdateUser(User user);
+    Task<User> UpdateUserById(Guid id, User user);
+    Task<User> DeleteUser(Guid id);
+}
