7559507e9175928be5ca942e405819371bc8c79b
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-05 06:40:48 +0300

    Refactor Create method, added GetUserById, UpdateUser

--- Forum.API/Controllers/UsersController.cs
+++ Forum.API/Controllers/UsersController.cs
@@ -1,33 +1,85 @@
 ﻿using Forum.API.DTOs;
+using Forum.API.Contracts;
 using Forum.Application.Interfaces.Repositories;
+using Forum.Application.Interfaces.Services;
 using Forum.Application.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Forum.API.Controllers
+namespace Forum.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly IUsersService _usersService;
+
+    public UsersController(IUsersService usersService)
     {
-        private readonly IUsersRepository _usersRepository;
+        _usersService = usersService;
+    }
 
-        public UsersController(IUsersRepository usersRepository)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserResponse>>> GetAllUsers()
+    {
+        IEnumerable<User> users = await _usersService.GetAllUsers();
+        IEnumerable<UserResponse> response = users.Select(u => new UserResponse(u.Id, u.Nickname, u.Email));
+        return Ok(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserResponse>> GetUserById(Guid id)
+    {
+        try
         {
-            _usersRepository = usersRepository;
+            User user = await _usersService.GetUserById(id);
+            UserResponse response = new UserResponse(user.Id, user.Nickname, user.Email);
+            return Ok(response);
         }
-
-        [HttpGet]
-        public ActionResult<string> Get()
+        catch (Exception ex)
         {
-            return Ok("User information");
+            return NotFound(ex.Message);
         }
+    }
 
-        [HttpPost]
-        public async Task<User> Create(CreateUserDto createUserDto)
+    [HttpPost]
+    public async Task<ActionResult<UserResponse>> CreateUser(CreateUserDto createUserDto)
+    {
+        try
         {
-            User userModel = Forum.Application.Models.User.CreateFromDto(createUserDto.Nickname, createUserDto.Email, createUserDto.Password, createUserDto.RegisterIp);
-            return await _usersRepository.CreateUser(userModel);
+            User userModel = Forum.Application.Models.User.CreateFromDto(
+            createUserDto.Nickname,
+            createUserDto.Email,
+            createUserDto.Password,
+            createUserDto.RegisterIp
+            );
+            User user = await _usersService.CreateUser(userModel);
+            UserResponse response = new UserResponse(user.Id, user.Nickname, user.Email);
+            return Ok(response);
+        }
+        catch(Exception ex) 
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<UserResponse>> UpdateUser(UpdateUserDto updateUserDto)
+    {
+        try
+        {
+            User userModel = await _usersService.GetUserById(updateUserDto.Id);
+            userModel.Nickname = updateUserDto.Nickname ?? userModel.Nickname;
+            userModel.Email = updateUserDto.Email ?? userModel.Email;
+            userModel.Password = updateUserDto.Password ?? userModel.Password;
+
+            User userUpdate = await _usersService.CreateUser(userModel);
+            UserResponse response = new UserResponse(userUpdate.Id, userUpdate.Nickname, userUpdate.Email);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
         }
     }
 }
