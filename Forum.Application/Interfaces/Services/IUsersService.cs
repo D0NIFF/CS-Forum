03cdcb42cd1b6043b6f60a7dac935835e0e7d71c
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-05 06:42:06 +0300

    UsersService

---
+++ Forum.Application/Interfaces/Services/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using Forum.Application.Models;
+
+namespace Forum.Application.Interfaces.Services;
+
+public interface IUsersService
+{
+    Task<IEnumerable<User>> GetAllUsers();
+    Task<User> GetUserById(Guid id);
+    Task<User> GetUserByNickname(string nickname);
+    Task<User> GetUserByEmail(string email);
+    Task<User> CreateUser(User user);
+    Task<User> UpdateUser(User user);
+    Task<bool> DeleteUser(Guid id);
+}
