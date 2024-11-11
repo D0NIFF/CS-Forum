9c8ce057ecbda531abe9ea3ef97eb47c2149147e
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-11 04:24:59 +0300

    Added interface for the service of post category

---
+++ Forum.Application/Interfaces/Services/IPostCategoriesService.cs
@@ -0,0 +1,12 @@
+﻿using Forum.Application.Models;
+
+namespace Forum.Application.Interfaces.Services;
+
+public interface IPostCategoriesService
+{
+    Task<IEnumerable<PostCategory>> GetAllPostCategories();
+    Task<PostCategory> GetPostCategoryById(Guid id);
+    Task<PostCategory> CreatePostCategory(PostCategory postCategory);
+    Task<PostCategory> UpdatePostCategory(PostCategory postCategory);
+    Task<bool> DeletePostCategory(Guid id);
+}
