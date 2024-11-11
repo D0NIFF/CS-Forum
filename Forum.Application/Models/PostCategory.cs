428fd32e746a982636be026b76a4c0952dd0cd8b
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-11 04:27:14 +0300

    Added 2 methods: makeFromEntity and makeFromDto

--- Forum.Application/Models/PostCategory.cs
+++ Forum.Application/Models/PostCategory.cs
@@ -1,4 +1,6 @@
-﻿namespace Forum.Application.Models;
+﻿using Forum.Domain.Entities;
+
+namespace Forum.Application.Models;
 
 public class PostCategory
 {
@@ -7,4 +9,24 @@ public class PostCategory
     public string Title { get; set; }
 
     public List<Post> Posts { get; init; } = new List<Post>();
+
+    public static PostCategory CreateFromEntity(PostCategoryEntity entity)
+    {
+        return new PostCategory()
+        {
+            Id = entity.Id,
+            Title = entity.Title
+        };
+    }
+
+    public static PostCategory CreateFromDto(string title)
+    {
+        PostCategory postCategory = new PostCategory()
+        {
+            Id = Guid.NewGuid(),
+            Title = title
+        };
+
+        return postCategory;
+    }
 }
