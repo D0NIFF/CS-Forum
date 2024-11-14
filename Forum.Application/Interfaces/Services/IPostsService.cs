c605435027053645aba43c91fc9c44cc6a1bdaef
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-14 06:49:30 +0300

    Create interface for user service

---
+++ Forum.Application/Interfaces/Services/IPostsService.cs
@@ -0,0 +1,12 @@
+﻿using Forum.Application.Models;
+
+namespace Forum.Application.Interfaces.Services;
+
+public interface IPostsService
+{
+    Task<IEnumerable<Post>> GetAllPosts();
+    Task<Post> GetPostById(Guid id);
+    Task<Post> CreatePost(Post post);
+    Task<Post> UpdatePost(Post post);
+    Task<bool> DeletePost(Guid id);
+}
