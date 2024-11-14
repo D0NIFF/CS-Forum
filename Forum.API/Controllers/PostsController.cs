b69d4e0f9a0f13fe4e4c2e1bdf25dcf4010d3378
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-14 06:48:58 +0300

    Create controller for Post intaractions

---
+++ Forum.API/Controllers/PostsController.cs
@@ -0,0 +1,92 @@
+﻿using Forum.API.Contracts;
+using Forum.API.DTOs;
+using Forum.Application.Interfaces.Repositories;
+using Forum.Application.Interfaces.Services;
+using Forum.Application.Models;
+using Forum.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostsService _postsService;
+        private readonly IUsersService _usersService;
+        private readonly IPostCategoriesService _postCategoriesService;
+
+        public PostsController(IPostsService postsService, IUsersService usersService, IPostCategoriesService postCategoriesService)
+        {
+            _postsService = postsService;
+            _usersService = usersService;
+            _postCategoriesService = postCategoriesService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PostResponse>>> GetAllPosts()
+        {
+            IEnumerable<Post> posts = await _postsService.GetAllPosts();
+            IEnumerable<PostResponse> response = posts.Select(p => new PostResponse(p.Id, p.Title, p.Description, p.CreatedAt));
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PostResponse>> GetPostById(Guid id)
+        {
+            try
+            {
+                Post post = await _postsService.GetPostById(id);
+                PostResponse response = new PostResponse(post.Id, post.Title, post.Description, post.CreatedAt);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PostResponse>> CreatePost(CreatePostDto createPostDto)
+        {
+            try
+            {
+                ActionResult<IEnumerable<UserResponse>> users = await _usersService.GetAllUsers();
+                Post postModel = Post.CreateFromDto(createPostDto.Title, createPostDto.Description, );
+                Post post = await _postsService.CreatePost(postModel);
+                PostResponse response = new PostResponse(postCategory.Id, postCategory.Title);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PostCategoryResponse>> UpdatePostCategory(Guid id, string title)
+        {
+            try
+            {
+                PostCategory postCategoryModel = await _postCategoriesService.GetPostCategoryById(id);
+                postCategoryModel.Title = title ?? postCategoryModel.Title;
+
+                PostCategory postCategoryUpdate = await _postCategoriesService.CreatePostCategory(postCategoryModel);
+                PostCategoryResponse response = new PostCategoryResponse(postCategoryUpdate.Id, postCategoryUpdate.Title);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePostCategory(Guid id)
+        {
+            await _postCategoriesService.DeletePostCategory(id);
+            return Ok();
+        }
+    }
+}
