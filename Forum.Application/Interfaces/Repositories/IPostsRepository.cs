e81f66e38d5df08c3137a3c2f5ff75ecec48e68e
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-14 06:48:43 +0300

    Create interface for post CRUD

--- Forum.Application/Interfaces/Repositories/IPostsRepository.cs
+++ Forum.Application/Interfaces/Repositories/IPostsRepository.cs
@@ -4,9 +4,9 @@ namespace Forum.Application.Interfaces.Repositories;
 
 public interface IPostsRepository
 {
-    Task<User> GetById(Guid id);
-    Task<IEnumerable<User>> Get();
-    Task<User> Create(User user);
-    Task<User> Update(User user);
+    Task<Post> GetById(Guid id);
+    Task<IEnumerable<Post>> Get();
+    Task<Post> Create(Post post);
+    Task<Post> Update(Post post);
     Task<bool> Delete(Guid id);
 }
