c873dd95986fb26b3160d6254c2e09709a9ec10a
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-14 06:49:53 +0300

    Create the user service

---
+++ Forum.Application/Services/PostsService.cs
@@ -0,0 +1,40 @@
+﻿using Forum.Application.Interfaces.Repositories;
+using Forum.Application.Interfaces.Services;
+using Forum.Application.Models;
+
+namespace Forum.Application.Services;
+
+public class PostsService : IPostsService
+{
+    private readonly IPostsRepository _postsRepository;
+
+    public PostsService(IPostsRepository postsRepository)
+    {
+        _postsRepository = postsRepository;
+    }
+
+    async Task<Post> IPostsService.CreatePost(Post post)
+    {
+        return await _postsRepository.Create(post);
+    }
+
+    async Task<bool> IPostsService.DeletePost(Guid id)
+    {
+        return await _postsRepository.Delete(id);
+    }
+
+    async Task<IEnumerable<Post>> IPostsService.GetAllPosts()
+    {
+        return await _postsRepository.Get();
+    }
+
+    async Task<Post> IPostsService.GetPostById(Guid id)
+    {
+        return await _postsRepository.GetById(id);
+    }
+
+    async Task<Post> IPostsService.UpdatePost(Post post)
+    {
+        return await _postsRepository.Update(post);
+    }
+}
